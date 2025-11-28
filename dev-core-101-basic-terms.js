bb7393cadb18e394f8709aa22cf695ee7e511906
Olzhas <oljas19042000@gmail.com>
2025-11-28 21:30:01 +0500

    Add files via upload

---
+++ dev-core-101-basic-terms.js
@@ -0,0 +1,28 @@
+let age = 25
+
+function saywelcome() {
+    console.log("Добро пожаловать!");
+}
+saywelcome(); // Вызов функции
+
+if (age >= 21) {
+    console.log("Вы можете купить алкоголь");
+} else {
+    console.log("Вы не можете купить алкоголь");
+}
+
+for (let i = 0; i < 5; i++) {
+    console.log(i); 
+}
+
+let x = 10;
+
+let result = 2 + 3; 
+
+let number = 10;
+
+let name = "Olzhas";
+
+// Это комментарий, он не выполняется
+
+let isAdult = true; // Логическое значение в JavaScript
